a71db9dbde3110991328f16dfe89a09a5ce67201
devcami <juf1015@gmail.com>
2022-05-09 10:15:03 +0900

    hello-git project init
    세부내용1
    세부내용2
    세부내용3

---
+++ hello-git/src/com/kh/git/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.kh.git;
+/**
+ * SCM Software Configuration Management
+ *  - 코드추적(버전관리) 외에 build, packaging, deploy 등 프로젝트 관련 프로세스를 관리
+ * 
+ * VCS Version Control System
+ *  - 프로젝트 작성/수정/보완에 따른 변경내역을 버젼으로 관리
+ *  
+ * 버젼관리 프로그램
+ *  - sever-client : 중앙저장소를 공유한 각각의 클라이언트에서 저장소의 일부만 가져와서 작업 후 중앙저장소에 반영
+ *  	- subversion (옛날스타일)
+ *  - 분산형 : 참여하는 각각의 클라이언트에서 전체저장소에 대한 복제본을 가지고 작업.
+ *  	- git
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+	}
+}
